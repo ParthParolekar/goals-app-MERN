3fb389cdff1ece7d8e2010b070f0567c3513ddfa
ParthParolekar <parolekarparth@gmail.com>
2022-08-06 21:43:18 +0530

    add: goal routes

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,8 @@
-console.log("Hello World");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.listen(port);
+
+app.use("/api/goals", require("./routes/goalRoutes"));
