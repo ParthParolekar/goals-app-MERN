900449bcff856605e5606d5828ae2f58d5d4a7cd
parth <parolekarparth@gmail.com>
2022-10-16 21:30:33 +0530

    Error handler middleware

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const app = express();
+const { errorHandler } = require("./middleware/errorMiddleware");
 const port = process.env.PORT || 5000;
 
-app.listen(port);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use("/api/goals", require("./routes/goalRoutes"));
+app.use(errorHandler);
+
+app.listen(port);
