affe92d0c4f58c44b685096272883576d8c93117
parth <parolekarparth@gmail.com>
2022-10-16 23:07:54 +0530

    connected with mongoDB and goal routes CRUD operations complete

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const colors = require("colors");
 const app = express();
 const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./config/db");
+
 const port = process.env.PORT || 5000;
+connectDB();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
